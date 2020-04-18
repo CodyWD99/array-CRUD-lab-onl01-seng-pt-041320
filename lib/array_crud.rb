42cffff9c43d1345e6469a7cc54f05e241d33d45
Cody Dupuis <codydupuis99@gmail.com>
2020-04-18 19:55:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Maiden", "Sabbath", "Megadeth", "Slayer"]
-  
+  add_element_to_end_of_array << "Iced Earth"
 end
 
 def add_element_to_start_of_array(array, element)
